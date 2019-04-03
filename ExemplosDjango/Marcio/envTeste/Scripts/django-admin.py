4146e2edca2cb1fcd56cc31636d7f26e2da4f611
marciorasf <marciorasf@gmail.com>
2019-04-02 23:32:41 -0300

    Adicao de projeto inicial com django
    Exemplo de um projeto django

---
+++ ExemplosDjango/Marcio/envTeste/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\repositoriowindows\github\projetoengsoftwarerepository\exemplosdjango\marcio\envteste\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
