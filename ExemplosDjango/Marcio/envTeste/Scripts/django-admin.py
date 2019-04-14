dc195807f28a461233130686470577ee7c2e93b6
marciorasf <marciorasf@gmail.com>
2019-04-14 02:33:04 -0300

    nova forma de repositorio

--- ExemplosDjango/Marcio/envTeste/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\repositoriowindows\github\projetoengsoftwarerepository\exemplosdjango\marcio\envteste\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
