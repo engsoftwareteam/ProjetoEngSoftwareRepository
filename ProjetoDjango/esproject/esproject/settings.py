635565f6cd93669dae2c70f8bd1b348e02ccb90e
marciorasf <marciorasf@gmail.com>
2019-04-14 02:00:00 -0300

    configuracoes para deploy no heroku

--- ProjetoDjango/esproject/esproject/settings.py
+++ ProjetoDjango/esproject/esproject/settings.py
@@ -117,5 +117,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
