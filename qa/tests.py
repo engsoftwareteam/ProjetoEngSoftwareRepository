63c112841a68e1d951869f402250730576a9d995
unknown <luhr_eis@hotmail.com>
2019-06-02 15:30:57 -0300

    first test running

--- qa/tests.py
+++ qa/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import UserProfileInfo
 
 # Create your tests here.
+class userTest(TestCase):
+    def test_registrar_usuario(self):
+        req_data = {'username': 'user@test.com','password': 'secret'}
+        response = self.client.post('/registrar_usuario', req_data)
+        self.assertEquals(response.status_code, 200)
