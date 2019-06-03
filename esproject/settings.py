4c78ef07b67c8ace1ce8f41f4401d89bb4e189b0
marciorasf <marciorasf@gmail.com>
2019-06-03 04:17:54 -0300

    testing travis

--- esproject/settings.py
+++ esproject/settings.py
@@ -118,4 +118,10 @@ USE_TZ = True
 LOGIN_URL = '/login_usuario'
 STATIC_URL = '/static/'
 
-django_heroku.settings(locals())
+
+try:
+    # Configure Django App for Heroku.
+    import django_heroku
+    django_heroku.settings(locals())
+except ImportError:
+    found = False
